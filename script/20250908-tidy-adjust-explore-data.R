e5c789676f0dc3d9cfa7c325e2544706c73cbccc
Dorsa-ij <dir016@uib.no>
2025-09-08 10:42:54 +0200

    New script for tidying and adjusting the exam_data

---
+++ script/20250908-tidy-adjust-explore-data.R
@@ -0,0 +1,9 @@
+#----Script Header-------------------------####
+# Date: 08.09.2025         
+# Author: Data_drama_queens     
+# Filename: tidy-adjust-explore-data   
+# Description: Script for tidying data
+#               
+#               
+# Project: RMED901A_Exam_Assignment
+#-------------------------------------------###
