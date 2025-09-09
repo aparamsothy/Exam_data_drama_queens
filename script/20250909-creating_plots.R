fc2305c860882ae8745e76b77a547d2e1203c47c
Dorsa-ij <dir016@uib.no>
2025-09-09 14:22:41 +0200

    change

--- script/20250909-creating_plots.R
+++ script/20250909-creating_plots.R
@@ -26,8 +26,16 @@ library(ggplot2)
 # Select only numeric columns for correlation
 num_data <- patient_data[sapply(patient_data, is.numeric)]
 # Correlation matrix plot
-correlation_plot <- ggcorr(num_data, label = FALSE)
+correlation_plot <- ggcorr(num_data, angle = 90, size = 3, clip = "off", label = FALSE) +
+  theme(
+    plot.margin = margin(t = 20, r = 40, b = 40, l = 40)  # top, right, bottom, left margins
+  )
+
+print(correlation_plot)
+
 
 # To display the plot
 print(correlation_plot)
+ggsave("correlation_plot.png", plot = correlation_plot, width = 12, height = 12, dpi = 300)
+
 
