11207f7d4ac238c8b41fdc9cac7d9d3867d8610c
AnnaSHessevik <annasunniva@hotmail.com>
2025-09-09 14:12:38 +0200

    Create boxplot for percentage of monocytes in WBC count grouped by remission status

--- script/20250909-creating_plots.R
+++ script/20250909-creating_plots.R
@@ -18,3 +18,32 @@ patient_data <- read_delim(
   here("data", "data_for_analysis", "20250908-tidy-joined-exam-data.txt"),
   delim = "\t"
 )
+
+# Remission and percent of monocytes ----
+# Here, we investigates if remission of inflammation after Thiopurines for > 12 weeks 
+# change with percent of monocytes in WBC count.
+#Create a boxplot
+remission_mono_plot <- ggplot(data=patient_data, 
+                              mapping=aes(x=remission, y=mono_percent)) + #select variables
+  geom_boxplot(fill=c("#7876B199", "#20854E99")) + #create a box plot and set specific colours
+  xlab("Remission") + #change x axis label
+  ylab("Monocytes in WBC count (%)") + #y axis labels
+  labs(title = "Percent of monocytes in WBC count",
+       subtitle = "Grouped by remission status") +
+  theme_minimal() + #use the minimal theme
+  theme(
+    panel.grid.major = element_blank(), #remove major grid
+    panel.grid.minor = element_blank(), #remove minor grid
+    axis.line = element_line(colour="grey", linewidth = 0.5), #include x and y axis
+    axis.ticks = element_line(colour = "grey", linewidth = 0.5), #include tick marks
+    legend.position = "None", #remove legend
+    plot.title = element_text(size = 16, face = "bold"), #change title font
+    axis.title = element_text(size = 14), #change axis title size
+    axis.text.x = element_text( size = 12), #change x-axis text size
+    axis.text.y = element_text(size=12) #change y-axis text size
+  ) 
+  
+  
+  
+remission_mono_plot
+
