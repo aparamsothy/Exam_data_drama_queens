5eec46514b909817e1805f32586c84933da123b9
Abira1212 <abira_1212@hotmail.com>
2025-09-10 12:09:40 +0200

    Statistical analysis to investigate if remission is depended on chloride. Counted individuals in each group, created a boxplot to visualize and performed a t-test.

--- script/20250910-statistical-analysis.R
+++ script/20250910-statistical-analysis.R
@@ -20,3 +20,44 @@ patient_data <- read_delim(
   here("data", "data_for_analysis", "20250908-tidy-joined-exam-data.txt"),
   delim = "\t"
 )
+
+# Statistical test to investigate if the remission is depended on chloride
+
+## Count the number of patients in each category of remission
+patient_data %>%
+  group_by(remission) %>%
+  count()
+
+## Boxplot to visualize chloride levels by remission status.
+ggplot(patient_data, aes(remission, chloride, color = remission)) +
+  geom_boxplot() +
+  theme_minimal() +
+  theme(
+    legend.position = "none"
+  ) +
+  labs(title = "Chloride level by Remission Status",
+       x = "Chloride level",
+       y = "Remission status") +
+  theme(
+    axis.title.x = element_text(face = "bold", size = 9),
+    axis.title.y = element_text(face = "bold", size = 9),
+    title = element_text(face = "bold", hjust = 0.5)
+  ) +
+  scale_color_manual(values = c("No" = "blue", "Yes" = "orange"))
+
+# T-test to test difference in mean chloride levels by remission status
+t_test_results <- patient_data %>% 
+  t.test(chloride ~ remission, data = .) %>%
+  broom::tidy()
+
+t_test_results
+# We have used an unpaired t-test for statistical analysis because we are
+# interested in comparing the mean of chloride level between those with and 
+# without remission. 
+
+# The 2808 patients with remission compared to the 2359 patients
+# without remission showed significantly lower chloride levels (t = -0.377, p = 0.000113)
+
+
+  
+
