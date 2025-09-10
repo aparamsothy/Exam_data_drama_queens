3dd6082488042e0245d61fa44c1199ae6cd89218
Dorsa-ij <dir016@uib.no>
2025-09-10 10:54:18 +0200

    Script header

---
+++ script/20250910-statistical-analysis.R
@@ -0,0 +1,15 @@
+#----Script Header-------------------------------------------------------------####
+# Date: 10.09.2025         
+# Author: Data_drama_queens     
+# Filename: creating-plots
+# Description: Script for creating plots.
+#              We were given 5 questions, and asked to select one question each.
+#              We have answered the following questions:
+#                 - Are there any correlated measurements?
+#                 - Does the white blood cell count distribution depend on pot?
+#                 - Does remission of inflammation after Thiopurines for > 12 
+#                   weeks change with percent of monocytes in WBC count?
+#               
+# Project: RMED901A_Exam_Assignment
+#-------------------------------------------------------------------------------###
+
